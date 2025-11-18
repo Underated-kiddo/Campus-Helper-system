26e47258c5a6fee717dc5855fd049b50a7f7957a
Underated-kiddo <lawrencebradley231@gmail.com>
2025-11-18 00:44:51 -0800

    deployed

--- Server/server.js
+++ Server/server.js
@@ -7,7 +7,10 @@ const path = require("path");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+	origin: process.env.CLIENT_URL,
+	credentials: true,
+    }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
