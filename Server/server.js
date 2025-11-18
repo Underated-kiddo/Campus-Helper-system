faef55cdd747160e16975a1df98255e41804fba9
Underated-kiddo <lawrencebradley231@gmail.com>
2025-11-18 15:26:18 -0800

    nearly done

--- Server/server.js
+++ Server/server.js
@@ -8,9 +8,11 @@ const app = express();
 
 // Middleware
 app.use(cors({
-	origin: process.env.CLIENT_URL,
-	credentials: true,
-    }));
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+    allowedHeaders: ["Content-Type","Authorization"],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
